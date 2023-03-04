35a87127c66b99e42a1f7f5450d61a2d379422f9
Aditya Singh <124086655+adityaIIITR2062@users.noreply.github.com>
2023-03-05 00:52:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ saveButton.addEventListener('click', function() {
 deleteButton.addEventListener('click', function() {
     textbox.value = '';
 });
+
+const textbox1 = document.getElementById('textbox1');
+const editButton1 = document.getElementById('edit-button1');
+const saveButton1 = document.getElementById('save-button1');
+const deleteButton1 = document.getElementById('delete-button1');
+
+editButton1.addEventListener('click', function() {
+    textbox1.removeAttribute('readonly');
+    editButton1.style.display = 'none';
+    saveButton1.style.display = 'inline-block';
+});
+
+saveButton1.addEventListener('click', function() {
+    textbox1.setAttribute('readonly', true);
+    editButton1.style.display = 'inline-block';
+    saveButton1.style.display = 'none';
+});
+
+deleteButton1.addEventListener('click', function() {
+    textbox1.value = '';
+});
+
+
